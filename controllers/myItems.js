fd15c9d541cca7cb70dccd327f00587f3edfd72c
lauren robinson <laurenrobinson@laurens-MacBook-Air.local>
2019-09-27 12:46:24 -0700

    updated files, connected to show hbs

--- controllers/myItems.js
+++ controllers/myItems.js
@@ -1,31 +1,49 @@
 const express = require("express");
 const router = express.Router();
-
 const TweetModel = require('../models/Tweets.js');
 
+
+// initial home route
 router.get("/", (req,res) => {
     TweetModel.find({}).then(myItems => res.render('index', { myItems }))
+    .catch(err => console.log(err))
 })
+
+//route to new hbs to create new tweet
 router.get("/new", (req, res) => {
     res.render('new');
 })
 
+//route to post new created tweet
 router.post('/', (req, res) => {
     TweetModel.create(req.body)
     .then(myNewItem => {
         res.redirect('/');
     })
 })
-router.get('/edit/:id', (req, res) => {
-    TweetModel.findOne({_id: req.params.id}).then(instance => {
-        res.render("edit", { instance })
-    })
-})
 
-router.get("/:id", (req,res) => {
+//route to show to edit existing tweet
+router.get("/show", (req,res) => {
     TweetModel.findOne({ _id: req.params.id}).then(myItems => res.render('show', {myItems}));
 })
 
-
-
+router.get('/edit/:id', (req, res) => {
+    TweetModel.findOne({_id: req.params.id})
+      .then(instance => {
+        res.render("edit", { myItems })
+      })
+      .catch(err => console.log(err))
+  })
+  router.put('/:id', (req, res) => {
+    TweetModel.findByIdAndUpdate({_id: req.params.id}, req.body, { new: true })
+      .then(myItems => {
+        res.redirect('/')
+      })
+      .catch(err => console.log(err))
+  });
+  router.delete('/:id', (req, res) => {
+      TweetModel.findOneAndRemove({ _id: req.params.id}).then(()=> {
+          res.redirect('/');
+      })
+  })
 module.exports = router;
--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-const express = require('express');
-const hbs = require('hbs');
-const myController = require('./controllers/myItems.js')
-const parser = require('body-parser');
-const methodOverride = require('method-override')
-
-
+const express = require("express");
+const hbs = require("hbs");
+const parser = require("body-parser");
+const myController = require("./controllers/myItems.js");
+const methodOverride = require("method-override");
 
 const app = express();
-app.use(parser.urlencoded({ extended: true }))
-app.use(methodOverride('_method'))
+app.use(parser.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 app.set("view engine", "hbs");
+app.get(':id', function(req, res){
+res.send('id: ' + req.params.id)
+});
+
+app.use("/",myController);
+
 app.listen(2000, () => console.log("Running on port 2000!"));
 
-app.use("/", myController);
